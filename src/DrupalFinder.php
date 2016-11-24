c5bf606cc0087eb197d4741b2e56b74abe00757e
Florian Weber <florian@webflo.org>
2016-11-24 20:58:26 +0100

    Applied fixes from StyleCI (#8)

--- src/DrupalFinder.php
+++ src/DrupalFinder.php
@@ -7,7 +7,8 @@
 
 namespace DrupalFinder;
 
-class DrupalFinder {
+class DrupalFinder
+{
 
   /**
    * Drupal web public directory.
@@ -23,34 +24,34 @@ class DrupalFinder {
    */
   private $composerRoot;
 
-  public function locateRoot($start_path) {
-    $this->drupalRoot = FALSE;
-    $this->composerRoot = FALSE;
+    public function locateRoot($start_path)
+    {
+        $this->drupalRoot = false;
+        $this->composerRoot = false;
 
-    foreach (array(TRUE, FALSE) as $follow_symlinks) {
-      $path = $start_path;
-      if ($follow_symlinks && is_link($path)) {
-        $path = realpath($path);
-      }
+        foreach (array(true, false) as $follow_symlinks) {
+            $path = $start_path;
+            if ($follow_symlinks && is_link($path)) {
+                $path = realpath($path);
+            }
       // Check the start path.
       if ($this->isValidRoot($path)) {
-        return TRUE;
-      }
-      else {
-        // Move up dir by dir and check each.
+          return true;
+      } else {
+          // Move up dir by dir and check each.
         while ($path = $this->shiftPathUp($path)) {
-          if ($follow_symlinks && is_link($path)) {
-            $path = realpath($path);
-          }
-          if ($this->isValidRoot($path)) {
-            return TRUE;
-          }
+            if ($follow_symlinks && is_link($path)) {
+                $path = realpath($path);
+            }
+            if ($this->isValidRoot($path)) {
+                return true;
+            }
         }
       }
-    }
+        }
 
-    return FALSE;
-  }
+        return false;
+    }
 
   /**
    * Returns parent directory.
@@ -61,9 +62,10 @@ class DrupalFinder {
    * @return string|FALSE
    *   Parent path of given path or false when $path is filesystem root.
    */
-  private function shiftPathUp($path) {
-    $parent = dirname($path);
-    return in_array($parent, ['.', $path]) ? FALSE : $parent;
+  private function shiftPathUp($path)
+  {
+      $parent = dirname($path);
+      return in_array($parent, ['.', $path]) ? false : $parent;
   }
 
   /**
@@ -71,46 +73,48 @@ class DrupalFinder {
    *
    * @return boolean
    */
-  protected function isValidRoot($path) {
-    if (!empty($path) && is_dir($path) && file_exists($path . '/autoload.php') && file_exists($path . '/composer.json')) {
-      // Additional check for the presence of core/composer.json to
+  protected function isValidRoot($path)
+  {
+      if (!empty($path) && is_dir($path) && file_exists($path . '/autoload.php') && file_exists($path . '/composer.json')) {
+          // Additional check for the presence of core/composer.json to
       // grant it is not a Drupal 7 site with a base folder named "core".
       $candidate = 'core/includes/common.inc';
-      if (file_exists($path . '/' . $candidate) && file_exists($path . '/core/core.services.yml')) {
-        if (file_exists($path . '/core/misc/drupal.js') || file_exists($path . '/core/assets/js/drupal.js')) {
-          $this->composerRoot = $path;
-          $this->drupalRoot = $path;
-        }
-      }
-    }
-    if (!empty($path) && is_dir($path) && file_exists($path . '/composer.json')) {
-      $json = json_decode(file_get_contents($path . '/composer.json'), TRUE);
-      if (is_array($json) && isset($json['require']['drupal/core'])) {
-        if (isset($json['extra']['installer-paths']) && is_array($json['extra']['installer-paths'])) {
-          foreach ($json['extra']['installer-paths'] as $install_path => $items) {
-            if (in_array('type:drupal-core', $items)) {
-              $this->composerRoot = $path;
-              $this->drupalRoot = $path . '/' . substr($install_path, 0, -5);
-            }
+          if (file_exists($path . '/' . $candidate) && file_exists($path . '/core/core.services.yml')) {
+              if (file_exists($path . '/core/misc/drupal.js') || file_exists($path . '/core/assets/js/drupal.js')) {
+                  $this->composerRoot = $path;
+                  $this->drupalRoot = $path;
+              }
           }
-        }
       }
-    }
-    return ($this->drupalRoot && $this->composerRoot);
+      if (!empty($path) && is_dir($path) && file_exists($path . '/composer.json')) {
+          $json = json_decode(file_get_contents($path . '/composer.json'), true);
+          if (is_array($json) && isset($json['require']['drupal/core'])) {
+              if (isset($json['extra']['installer-paths']) && is_array($json['extra']['installer-paths'])) {
+                  foreach ($json['extra']['installer-paths'] as $install_path => $items) {
+                      if (in_array('type:drupal-core', $items)) {
+                          $this->composerRoot = $path;
+                          $this->drupalRoot = $path . '/' . substr($install_path, 0, -5);
+                      }
+                  }
+              }
+          }
+      }
+      return ($this->drupalRoot && $this->composerRoot);
   }
 
   /**
    * @return string
    */
-  public function getDrupalRoot() {
-    return $this->drupalRoot;
+  public function getDrupalRoot()
+  {
+      return $this->drupalRoot;
   }
 
   /**
    * @return string
    */
-  public function getComposerRoot() {
-    return $this->composerRoot;
+  public function getComposerRoot()
+  {
+      return $this->composerRoot;
   }
-
 }
--- tests/DrupalFinderTest.php
+++ tests/DrupalFinderTest.php
@@ -2,14 +2,15 @@
 
 use org\bovigo\vfs\vfsStream;
 
-class DrupalFinderTest extends PHPUnit_Framework_TestCase {
+class DrupalFinderTest extends PHPUnit_Framework_TestCase
+{
 
   /**
    * @var \DrupalFinder\DrupalFinder
    */
   protected $finder;
 
-  protected static $fileStructure = [
+    protected static $fileStructure = [
     'autoload.php' => '',
     'composer.json' => '',
     'core' => [
@@ -27,8 +28,9 @@ class DrupalFinderTest extends PHPUnit_Framework_TestCase {
   /**
    * @return array
    */
-  protected function getDrupalComposerStructure() {
-    $fileStructure = [
+  protected function getDrupalComposerStructure()
+  {
+      $fileStructure = [
       'web' => static::$fileStructure,
       'composer.json' => json_encode([
         'require' => [
@@ -43,152 +45,160 @@ class DrupalFinderTest extends PHPUnit_Framework_TestCase {
         ],
       ])
     ];
-    unset($fileStructure['web']['composer.json']);
-    return $fileStructure;
+      unset($fileStructure['web']['composer.json']);
+      return $fileStructure;
   }
 
-  protected function setUp() {
-    parent::setUp();
-    $this->finder = new \DrupalFinder\DrupalFinder();
-  }
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->finder = new \DrupalFinder\DrupalFinder();
+    }
 
-  public function testDrupalDefaultStructure() {
-    $root = vfsStream::setup('root', null, static::$fileStructure);
+    public function testDrupalDefaultStructure()
+    {
+        $root = vfsStream::setup('root', null, static::$fileStructure);
 
-    $this->assertTrue($this->finder->locateRoot($root->url()));
-    $this->assertSame('vfs://root', $this->finder->getDrupalRoot());
-    $this->assertSame('vfs://root', $this->finder->getComposerRoot());
+        $this->assertTrue($this->finder->locateRoot($root->url()));
+        $this->assertSame('vfs://root', $this->finder->getDrupalRoot());
+        $this->assertSame('vfs://root', $this->finder->getComposerRoot());
 
-    $this->assertTrue($this->finder->locateRoot($root->url() . '/misc'));
-    $this->assertSame('vfs://root', $this->finder->getDrupalRoot());
-    $this->assertSame('vfs://root', $this->finder->getComposerRoot());
+        $this->assertTrue($this->finder->locateRoot($root->url() . '/misc'));
+        $this->assertSame('vfs://root', $this->finder->getDrupalRoot());
+        $this->assertSame('vfs://root', $this->finder->getComposerRoot());
 
-    $root = vfsStream::setup('root', null, ['project' => static::$fileStructure]);
-    $this->assertFalse($this->finder->locateRoot($root->url()), 'Not in the scope of the project');
-    $this->assertFalse($this->finder->getDrupalRoot());
-    $this->assertFalse($this->finder->getComposerRoot());
-  }
+        $root = vfsStream::setup('root', null, ['project' => static::$fileStructure]);
+        $this->assertFalse($this->finder->locateRoot($root->url()), 'Not in the scope of the project');
+        $this->assertFalse($this->finder->getDrupalRoot());
+        $this->assertFalse($this->finder->getComposerRoot());
+    }
 
-  public function testDrupalComposerStructure() {
-    $fileStructure = $this->getDrupalComposerStructure();
+    public function testDrupalComposerStructure()
+    {
+        $fileStructure = $this->getDrupalComposerStructure();
 
-    $root = vfsStream::setup('root', null, $fileStructure);
-    $this->assertTrue($this->finder->locateRoot($root->url() . '/web'));
-    $this->assertSame('vfs://root/web', $this->finder->getDrupalRoot());
-    $this->assertSame('vfs://root', $this->finder->getComposerRoot());
+        $root = vfsStream::setup('root', null, $fileStructure);
+        $this->assertTrue($this->finder->locateRoot($root->url() . '/web'));
+        $this->assertSame('vfs://root/web', $this->finder->getDrupalRoot());
+        $this->assertSame('vfs://root', $this->finder->getComposerRoot());
 
-    $this->assertTrue($this->finder->locateRoot($root->url() . '/web/misc'));
-    $this->assertSame('vfs://root/web', $this->finder->getDrupalRoot());
-    $this->assertSame('vfs://root', $this->finder->getComposerRoot());
+        $this->assertTrue($this->finder->locateRoot($root->url() . '/web/misc'));
+        $this->assertSame('vfs://root/web', $this->finder->getDrupalRoot());
+        $this->assertSame('vfs://root', $this->finder->getComposerRoot());
 
-    $this->assertTrue($this->finder->locateRoot($root->url()));
-    $this->assertSame('vfs://root/web', $this->finder->getDrupalRoot());
-    $this->assertSame('vfs://root', $this->finder->getComposerRoot());
+        $this->assertTrue($this->finder->locateRoot($root->url()));
+        $this->assertSame('vfs://root/web', $this->finder->getDrupalRoot());
+        $this->assertSame('vfs://root', $this->finder->getComposerRoot());
 
-    $root = vfsStream::setup('root', null, ['nested_folder' => $fileStructure]);
-    $this->assertFalse($this->finder->locateRoot($root->url()));
-    $this->assertFalse($this->finder->getDrupalRoot());
-    $this->assertFalse($this->finder->getComposerRoot());
-  }
+        $root = vfsStream::setup('root', null, ['nested_folder' => $fileStructure]);
+        $this->assertFalse($this->finder->locateRoot($root->url()));
+        $this->assertFalse($this->finder->getDrupalRoot());
+        $this->assertFalse($this->finder->getComposerRoot());
+    }
 
-  public function testNoDrupalRootWithRealFilesystem() {
-    $root = $this->tempdir(sys_get_temp_dir());
+    public function testNoDrupalRootWithRealFilesystem()
+    {
+        $root = $this->tempdir(sys_get_temp_dir());
 
-    $this->assertFalse($this->finder->locateRoot($root));
-    $this->assertFalse($this->finder->getDrupalRoot());
-    $this->assertFalse($this->finder->getComposerRoot());
-  }
+        $this->assertFalse($this->finder->locateRoot($root));
+        $this->assertFalse($this->finder->getDrupalRoot());
+        $this->assertFalse($this->finder->getComposerRoot());
+    }
 
-  public function testDrupalDefaultStructureWithRealFilesystem() {
-    $root = $this->tempdir(sys_get_temp_dir());
-    $this->dumpToFileSystem(static::$fileStructure, $root);
+    public function testDrupalDefaultStructureWithRealFilesystem()
+    {
+        $root = $this->tempdir(sys_get_temp_dir());
+        $this->dumpToFileSystem(static::$fileStructure, $root);
 
-    $this->assertTrue($this->finder->locateRoot($root));
-    $this->assertSame($root, $this->finder->getDrupalRoot());
-    $this->assertSame($root, $this->finder->getComposerRoot());
+        $this->assertTrue($this->finder->locateRoot($root));
+        $this->assertSame($root, $this->finder->getDrupalRoot());
+        $this->assertSame($root, $this->finder->getComposerRoot());
 
     // Test symlink implementation
     $symlink = $this->tempdir(sys_get_temp_dir());
-    $this->symlink($root, $symlink . '/foo');
+        $this->symlink($root, $symlink . '/foo');
 
-    $this->assertTrue($this->finder->locateRoot($symlink . '/foo'));
-    $this->assertSame($root, $this->finder->getDrupalRoot());
-    $this->assertSame($root, $this->finder->getComposerRoot());
-  }
+        $this->assertTrue($this->finder->locateRoot($symlink . '/foo'));
+        $this->assertSame($root, $this->finder->getDrupalRoot());
+        $this->assertSame($root, $this->finder->getComposerRoot());
+    }
 
-  public function testDrupalComposerStructureWithRealFilesystem() {
-    $root = $this->tempdir(sys_get_temp_dir());
-    $this->dumpToFileSystem($this->getDrupalComposerStructure(), $root);
+    public function testDrupalComposerStructureWithRealFilesystem()
+    {
+        $root = $this->tempdir(sys_get_temp_dir());
+        $this->dumpToFileSystem($this->getDrupalComposerStructure(), $root);
 
-    $this->assertTrue($this->finder->locateRoot($root));
-    $this->assertSame($root . '/web', $this->finder->getDrupalRoot());
-    $this->assertSame($root, $this->finder->getComposerRoot());
+        $this->assertTrue($this->finder->locateRoot($root));
+        $this->assertSame($root . '/web', $this->finder->getDrupalRoot());
+        $this->assertSame($root, $this->finder->getComposerRoot());
 
     // Test symlink implementation
     $symlink = $this->tempdir(sys_get_temp_dir());
-    $this->symlink($root, $symlink . '/foo');
+        $this->symlink($root, $symlink . '/foo');
 
-    $this->assertTrue($this->finder->locateRoot($symlink . '/foo'));
-    $this->assertSame($root . '/web', $this->finder->getDrupalRoot());
-    $this->assertSame($root, $this->finder->getComposerRoot());
-  }
-
-  public function testDrupalWithLinkedModule() {
-    $root = $this->tempdir(sys_get_temp_dir());
-    $this->dumpToFileSystem(static::$fileStructure, $root);
-
-    $module = $this->tempdir(sys_get_temp_dir());
-    $module_link = $root . '/modules/foo';
-    $this->symlink($module, $module_link);
-
-    $this->assertTrue($this->finder->locateRoot($module_link));
-    $this->assertSame($root, realpath($this->finder->getDrupalRoot()));
-    $this->assertSame($root, realpath($this->finder->getComposerRoot()));
-  }
-
-  protected function dumpToFileSystem($fileStructure, $root) {
-    foreach ($fileStructure as $name => $content) {
-      if (is_array($content)) {
-        mkdir($root . '/' . $name);
-        $this->dumpToFileSystem($content, $root . '/' . $name);
-      }
-      else {
-        file_put_contents($root . '/' . $name, $content);
-      }
+        $this->assertTrue($this->finder->locateRoot($symlink . '/foo'));
+        $this->assertSame($root . '/web', $this->finder->getDrupalRoot());
+        $this->assertSame($root, $this->finder->getComposerRoot());
     }
-  }
 
-  protected function tempdir($dir, $prefix = '', $mode = 0700) {
-    if (substr($dir, -1) != '/') {
-      $dir .= '/';
-    }
-    do {
-      $path = $dir . $prefix . mt_rand(0, 9999999);
-    }
-    while (!mkdir($path, $mode));
-    register_shutdown_function(['DrupalFinderTest', 'tempdir_remove'], $path);
-    return realpath($path);
-  }
+    public function testDrupalWithLinkedModule()
+    {
+        $root = $this->tempdir(sys_get_temp_dir());
+        $this->dumpToFileSystem(static::$fileStructure, $root);
 
-  static function tempdir_remove($path) {
-    if (is_link($path)) {
-      if (defined('PHP_WINDOWS_VERSION_BUILD')){
+        $module = $this->tempdir(sys_get_temp_dir());
+        $module_link = $root . '/modules/foo';
+        $this->symlink($module, $module_link);
+
+        $this->assertTrue($this->finder->locateRoot($module_link));
+        $this->assertSame($root, realpath($this->finder->getDrupalRoot()));
+        $this->assertSame($root, realpath($this->finder->getComposerRoot()));
+    }
+
+    protected function dumpToFileSystem($fileStructure, $root)
+    {
+        foreach ($fileStructure as $name => $content) {
+            if (is_array($content)) {
+                mkdir($root . '/' . $name);
+                $this->dumpToFileSystem($content, $root . '/' . $name);
+            } else {
+                file_put_contents($root . '/' . $name, $content);
+            }
+        }
+    }
+
+    protected function tempdir($dir, $prefix = '', $mode = 0700)
+    {
+        if (substr($dir, -1) != '/') {
+            $dir .= '/';
+        }
+        do {
+            $path = $dir . $prefix . mt_rand(0, 9999999);
+        } while (!mkdir($path, $mode));
+        register_shutdown_function(['DrupalFinderTest', 'tempdir_remove'], $path);
+        return realpath($path);
+    }
+
+    public static function tempdir_remove($path)
+    {
+        if (is_link($path)) {
+            if (defined('PHP_WINDOWS_VERSION_BUILD')) {
+                rmdir($path);
+            } else {
+                unlink($path);
+            }
+            return;
+        }
+
+        foreach (scandir($path) as $child) {
+            if (in_array($child, ['.', '..'])) {
+                continue;
+            }
+            $child = "$path/$child";
+            is_dir($child) ? static::tempdir_remove($child) : unlink($child);
+        }
         rmdir($path);
-      } else {
-        unlink($path);
-      }
-      return;
     }
-
-    foreach (scandir($path) as $child) {
-      if (in_array($child, ['.', '..'])) {
-        continue;
-      }
-      $child = "$path/$child";
-      is_dir($child) ? static::tempdir_remove($child) : unlink($child);
-    }
-    rmdir($path);
-  }
 
 
   /**
@@ -198,23 +208,23 @@ class DrupalFinderTest extends PHPUnit_Framework_TestCase {
    * @throws PHPUnit_Framework_SkippedTestError
    *
    */
-  private function symlink($target, $link) {
-    try {
-      return symlink($target, $link);
-    } catch (Exception $e) {
-      if (
+  private function symlink($target, $link)
+  {
+      try {
+          return symlink($target, $link);
+      } catch (Exception $e) {
+          if (
         defined('PHP_WINDOWS_VERSION_BUILD')
         && strstr($e->getMessage(), WIN_ERROR_PRIVILEGE_NOT_HELD)
       ) {
-        $this->markTestSkipped(<<<'MESSAGE'
+              $this->markTestSkipped(<<<'MESSAGE'
 No privilege to create symlinks. Run test as Administrator (elevated process).
 MESSAGE
 );
+          }
+          throw $e;
       }
-      throw $e;
-    }
   }
-
 }
 
 define('WIN_ERROR_PRIVILEGE_NOT_HELD', '1314');
