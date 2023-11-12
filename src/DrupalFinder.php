6504ac31e1a72ba41c45e2697dc5b6e2836e31b6
Moshe Weitzman <weitzman@tejasa.com>
2023-11-12 08:09:33 -0500

    Specify return types

--- src/DrupalFinder.php
+++ src/DrupalFinder.php
@@ -14,10 +14,9 @@ class DrupalFinder
     /**
      * Get the Drupal root.
      *
-     * @return string|bool
      *   The path to the Drupal root, if it was discovered. False otherwise.
      */
-    public function getDrupalRoot()
+    public function getDrupalRoot(): string|bool
     {
       $core = InstalledVersions::getInstallPath('drupal/core');
       return $core ? realpath(dirname($core)) : false;
@@ -26,10 +25,10 @@ class DrupalFinder
     /**
      * Get the Composer root.
      *
-     * @return string|bool
+     * @return
      *   The path to the Composer root, if it was discovered. False otherwise.
      */
-    public function getComposerRoot()
+    public function getComposerRoot(): string|bool
     {
         return realpath(dirname($this->getVendorDir()));
     }
@@ -37,17 +36,17 @@ class DrupalFinder
     /**
      * Get the vendor path.
      *
-     * @return string|bool
+     * @return
      *   The path to the vendor directory, if it was found. False otherwise.
      */
-    public function getVendorDir()
+    public function getVendorDir(): string|bool
     {
-      // See https://getcomposer.org/doc/07-runtime.md#autoloader-path-in-binaries
-      // PHPUnit replaces $_composer_autoload_path with its constant in vendor/phpunit/phpunit/phpunit
       $return = false;
       if (isset($GLOBALS['_composer_autoload_path'])) {
+        // See https://getcomposer.org/doc/07-runtime.md#autoloader-path-in-binaries
         $return = realpath(dirname($GLOBALS['_composer_autoload_path']));
       } elseif (defined('PHPUNIT_COMPOSER_INSTALL')) {
+        // PHPUnit replaces $_composer_autoload_path with its constant in vendor/phpunit/phpunit/phpunit
         $return = realpath(dirname(PHPUNIT_COMPOSER_INSTALL));
       }
       return $return;
