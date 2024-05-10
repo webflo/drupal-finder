1db08d1e2f75d2aee32575cbb95617b8e179a3a1
Florian Weber <florian.weber@ueberbit.de>
2024-05-10 23:02:03 +0200

    Add support for  from global variable

--- src/DrupalFinderComposerRuntime.php
+++ src/DrupalFinderComposerRuntime.php
@@ -25,6 +25,16 @@ class DrupalFinderComposerRuntime
      */
     public function getComposerRoot(): ?string
     {
+        if (isset($GLOBALS['_composer_autoload_path'])) {
+          $composerAutoloadPath = realpath($GLOBALS['_composer_autoload_path']);
+          if (is_string($composerAutoloadPath)) {
+            $installed = dirname($composerAutoloadPath) . '/composer/installed.php';
+            if (file_exists($installed)) {
+              $installed = require $installed;
+              return realpath($installed['root']['install_path']);
+            }
+          }
+        }
         $root = InstalledVersions::getRootPackage();
         return realpath($root['install_path']);
     }
@@ -34,6 +44,13 @@ class DrupalFinderComposerRuntime
      */
     public function getVendorDir(): ?string
     {
+      $installedVersions = new InstalledVersions();
+      if (isset($GLOBALS['_composer_autoload_path'])) {
+        $composerAutoloadPath = realpath($GLOBALS['_composer_autoload_path']);
+        if (is_string($composerAutoloadPath)) {
+          return dirname($composerAutoloadPath);
+        }
+      }
       $reflection = new \ReflectionClass(InstalledVersions::class);
       return realpath(dirname(dirname($reflection->getFileName())));
     }
